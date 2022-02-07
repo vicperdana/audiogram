f34aa88ea53f544ff1665bcb470bb7e53e2206c6
Ubuntu <viperdan@audiogram.efro3d1jmu0u3o2fttg43k1ufh.px.internal.cloudapp.net>
2022-02-07 11:24:02 +0000

    updates

--- audiogram/lib/transports/index.js
+++
@@ -1,6 +0,0 @@
-var extend = require("underscore").extend,
-    serverSettings = require("../settings/"),
-    s3 = require("./s3")(serverSettings),
-    redis = require("./redis")(serverSettings);
-
-module.exports = extend({}, redis, s3);
--- audiogram/lib/transports/redis/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(settings) {
-
-  if (settings.redisHost) {
-    return require("./remote")(settings.redisHost);
-  } else {
-    return require("./fake")();
-  }
-
-};
